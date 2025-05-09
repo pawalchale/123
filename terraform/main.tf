171503e5a09c471f9c4e5a0afd9ed6f407923618
pawalchale <88154576+pawalchale@users.noreply.github.com>
2025-05-09 12:34:08 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,7 +18,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "sa" {
-  name                     = "staticsiteqa123" # globally unique
+  name                     = "staticsite${var.env}123" # globally unique
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = var.location
   account_tier             = "Standard"
